dce9619f673c92144117b3c99110df51be282cad
MrPen <lan474444@gmail.com>
2025-07-06 03:17:45 +0700

    feat: add resume forms

--- src/components/forms/NewCVWithJDForm.tsx
+++ src/components/forms/NewCVWithJDForm.tsx
@@ -2,12 +2,12 @@
 
 import { useActionState, useEffect } from 'react'
 import InputField from '../InputField'
-import { createResume } from '@/lib/actions'
+import { createResumeWithJD } from '@/lib/actions'
 import { toast } from 'react-toastify'
 import { useRouter } from 'next/navigation'
 
-export default function NewCVForm() {
-	const [state, formAction] = useActionState(createResume, {
+export default function NewCVWithJDForm() {
+	const [state, formAction] = useActionState(createResumeWithJD, {
 		success: false,
 		error: false,
 		data: { id: '' },
@@ -24,9 +24,10 @@ export default function NewCVForm() {
 
 	return (
 		<div className="flex flex-col gap-4">
-			<div className="text-xl font-semibold underline">Thông tin:</div>
+			<div className="text-xl font-semibold underline">Thông tin chung của CV:</div>
 			<form action={formAction} className="flex flex-col gap-4">
 				<InputField name="title" label="Tên CV" />
+				<InputField name="description" label="Mô tả (JD) - Sử dụng AI để tự động hóa tạo CV" type="textarea" />
 				<button
 					className="rounded-xl bg-gray-800 px-3 py-2 text-sm font-semibold text-white hover:cursor-pointer hover:bg-gray-700"
 					type="submit"
